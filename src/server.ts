94661364ed636aca326823b899407c87fc53845d
preciousmakadho <preciousmakadho8@gmail.com>
2025-07-18 09:18:39 +0200

    authentication

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express, { Request, Response } from 'express';
 import { connectToDatabase } from './config/database';
 import dotenv from 'dotenv';
 import productRoutes from './routes/product.routes';
+import authRoutes from './routes/auth.routes';
 dotenv.config();
 const app = express();
 const PORT = 3000;
@@ -11,7 +12,7 @@ app.get('/', (req: Request, res: Response) => {
     res.json('Hello, World!');
 });
 app.use('/products', productRoutes);
-
+app.use('/auth', authRoutes);
 const startServer = async () => {
     try{
         await connectToDatabase();
